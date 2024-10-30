e00bec55a4222140f087cd57c86f520d78208bf7
marcodiaasf <marco.felipi@hotmail.com>
2024-10-30 11:11:14 -0300

    login - muda titulo

--- lib/login_usuario.dart
+++ lib/login_usuario.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Demo',
+      title: 'Need - Login',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
